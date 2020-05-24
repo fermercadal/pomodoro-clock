f9df3adbdd35af5ca66a6d7735aecae84d87bcbb
Fernando Mercadal <fermercadal@outlook.com>
2020-05-24 18:07:06 -0300

    Updates

---
+++ precache-manifest.7da354f348d44580ac2a51d6be5f3c8e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b75a14bac21259bb1b32c69f1a9353d",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "0693dd3007a8dd89b7c9",
+    "url": "/pomodoro-clock/static/css/main.876733c9.chunk.css"
+  },
+  {
+    "revision": "bf325e4024f5bfd7262e",
+    "url": "/pomodoro-clock/static/js/2.34df5515.chunk.js"
+  },
+  {
+    "revision": "0693dd3007a8dd89b7c9",
+    "url": "/pomodoro-clock/static/js/main.51d27fcc.chunk.js"
+  },
+  {
+    "revision": "9dc5c903bf20f88a2923",
+    "url": "/pomodoro-clock/static/js/runtime-main.28f84319.js"
+  },
+  {
+    "revision": "9f379bf995c691daf98f1343ca3dbf97",
+    "url": "/pomodoro-clock/static/media/icon-decrease.9f379bf9.svg"
+  },
+  {
+    "revision": "9002e7d7f20dd1f68fef0c8f77d0372f",
+    "url": "/pomodoro-clock/static/media/icon-increase.9002e7d7.svg"
+  },
+  {
+    "revision": "ef7a1bbbf3042a5423ffa615f3ade2c8",
+    "url": "/pomodoro-clock/static/media/icon-pause.ef7a1bbb.svg"
+  },
+  {
+    "revision": "7f77fb78db561dad504e469dc55a004c",
+    "url": "/pomodoro-clock/static/media/icon-play.7f77fb78.svg"
+  },
+  {
+    "revision": "99e5292c05bd0323cce27f12e5ce9944",
+    "url": "/pomodoro-clock/static/media/icon-reset.99e5292c.svg"
+  },
+  {
+    "revision": "a3c6c90d7d847d98ed68d36a337e8d6a",
+    "url": "/pomodoro-clock/static/media/sound.a3c6c90d.mp3"
+  }
+]);
